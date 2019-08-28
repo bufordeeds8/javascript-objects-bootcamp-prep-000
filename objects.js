d9926ef0ba6ba8e6662bb27ff9db37c2620010fc
Buford Eeds <bufordeeds8@gmail.com>
2019-08-28 20:55:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {
   'artist' : 'title'
 };
 
-function updatePlaylist (playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle})
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
 }
